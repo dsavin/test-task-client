47f2b1d69f8e81329ddae50c833e608cca88fdc3
Dmytro Savin <savin.dmitry@gmail.com>
2021-04-09 14:14:33 +0300

    404 and main template

---
+++ src/containers/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Tech Test by Dima</h1>
+        <p className="text-muted">Pets insurance quotation app</p>
+      </div>
+    </div>
+  );
+}
