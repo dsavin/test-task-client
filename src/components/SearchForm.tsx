89a56c72cbd8beea3ed94e4669f31f6af43f3279
Dmytro Savin <savin.dmitry@gmail.com>
2021-04-12 16:31:44 +0300

    Add server side interaction

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -8,13 +8,15 @@ import config from "../config";
 import Policy from "../types/policies";
 import Col from "react-bootstrap/esm/Col";
 import { Row } from "react-bootstrap";
+import Breed from "../types/breeds";
 
 export default function SearchForm() {
   const [policyId, setPolicyId] = useState(0);
+  const [breedId, setBreedId] = useState(0);
   const [pet, setPet] = useState("");
   const [age, setAge] = useState(0);
   const [excess, setExcess] = useState(true);
-  const [breeds, setBreeds] = useState([]);
+  const [breeds, setBreeds] = useState<Breed[] | undefined> (undefined);
   const [isLoading, setIsLoading] = useState(false);
   const [policies, setPolicies] = useState<Policy[] | undefined> (undefined)
 
@@ -43,6 +45,13 @@ export default function SearchForm() {
     return result?.Items;
   }
 
+  async function loadBreeds(pet: String) {
+    const result = await API.get("quotes", "/breeds/" + pet, {});
+    console.log(result?.Items)
+    setBreeds(result?.Items)
+    return result?.Items;
+  }
+
   async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     
@@ -60,10 +69,9 @@ export default function SearchForm() {
     return API.post("quotes", "/", {
       body: {
         policyId,
-        pet,
         age,
         excess,
-        breeds
+        breedId
       }
     });
   }
@@ -80,7 +88,7 @@ export default function SearchForm() {
               autoFocus
               as="select"
               value={policyId}
-              onChange={(e) => setPolicyId(parseInt(e.target.value))}>
+              onChange={(e) => setPolicyId(parseInt(e.currentTarget.value))}>
                 
               {policies?.map(({ policyId, name }) => (
                 <option value={policyId}>{name}</option>
@@ -95,7 +103,8 @@ export default function SearchForm() {
                 type="radio"
                 aria-label="radio 1"
                 label="Dog"
-                //onChange={handleChange}
+                name="pet"
+                onChange={(e) => loadBreeds(e.currentTarget.value.toString())}
                 //checked={kindOfStand === "design"}
               />
               <Form.Check
@@ -103,7 +112,8 @@ export default function SearchForm() {
                   type="radio"
                   aria-label="radio 2"
                   label="Cat"
-                  //onChange={(e) => setPet(e.target.)}>
+                  name="pet"
+                  onChange={(e) => loadBreeds(e.currentTarget.value.toString())}
                   // checked={kindOfStand === "food"}
                 />
             </Form.Group>
@@ -125,6 +135,16 @@ export default function SearchForm() {
             </Form.Group>
           </Col>
         </Form.Row>
+        <Form.Label>Breeds</Form.Label>
+          <Form.Control
+            autoFocus
+            onChange={(e) => setBreedId(parseInt(e.currentTarget.value))}
+            as="select">
+            
+            {breeds?.map(({ breedId, name }) => (
+              <option value={breedId}>{name}</option>
+            ))}
+          </Form.Control>
         <Form.Group as={Row}>
           <Col sm={{ span: 10, offset: 2 }}>
 
---
+++ src/types/breeds.ts
@@ -0,0 +1,8 @@
+interface Breed {
+  breedId: string;
+  name: string;
+  priceModifier: Number;
+  pet: String;
+}
+
+export default Breed;
